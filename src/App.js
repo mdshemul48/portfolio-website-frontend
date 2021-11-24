1da6892f65f28608604c51b5d6d004b345296c57
MD. Shimul <mdshemul480@gmail.com>
2021-11-24 20:35:46 +0600

    adding nav bar

--- src/App.js
+++ src/App.js
@@ -5,10 +5,17 @@ import Home from './components/Home/Home';
 import Dashboard from './components/Dashboard/Dashboard';
 import Projects from './components/Projects/Projects';
 
+// header and footer
+import NavBar from './components/shared/NavBar/NavBar';
+
+// styles
 import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <BrowserRouter>
+      <NavBar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/projects' element={<Projects />} />
