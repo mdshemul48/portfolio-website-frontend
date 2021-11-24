e61b7a2aea924fe79399218b56ce780c24faf7bb
MD. Shimul <mdshemul480@gmail.com>
2021-11-24 19:20:54 +0600

    adding routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+// components
+import Home from './components/Home/Home';
+import Dashboard from './components/Dashboard/Dashboard';
+import Projects from './components/Projects/Projects';
+
+import './App.css';
 function App() {
-  return <div className='App'>hello world</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
