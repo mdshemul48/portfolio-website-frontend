42a69aab14ea484804cf392ce381b6392295e376
MD. Shimul <mdshemul480@gmail.com>
2021-11-25 11:22:31 +0600

    adding 3d photo to hero area

--- src/components/Home/HeroArea/HeroArea.jsx
+++ src/components/Home/HeroArea/HeroArea.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Button, Col, Container, Row } from 'react-bootstrap';
 
-import HeroPicture from '../../../assets/031.png';
+import HeroPicture from '../../../assets/HeroAreaPicture.png';
 
 import './HeroArea.css';
 const HeroArea = () => {
   return (
-    <Container className='mt-2 hero-area'>
+    <Container className='mt-2 hero-area pt-4 px-5 pb-1'>
       <Row>
         <Col lg={6}>
           <div className='hero-textArea'>
-            <h1>Hi! I Am</h1>
-            <h1>MD. Abdul Sattar Shimul</h1>
+            <h1>Hay There,</h1>
+            <h1>I'm MD. Shimul</h1>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
               harum nisi rerum, aut aliquid quis at? Deleniti veritatis, qui
