9a0d946afc9adc50db271e80ac86e9857b4b6d6c
MD. Shimul <mdshemul480@gmail.com>
2021-11-25 18:06:52 +0600

    moving icons

--- src/components/Home/HeroArea/HeroArea.jsx
+++ src/components/Home/HeroArea/HeroArea.jsx
@@ -27,13 +27,13 @@ const HeroArea = () => {
                 className='me-2'
               />
               <img
-                src='https://img.shields.io/badge/Facebook-%231877F2.svg?style=for-the-badge&logo=Facebook&logoColor=white'
-                alt='facebook'
+                src='https://img.shields.io/badge/linkedin-%230077B5.svg?style=for-the-badge&logo=linkedin&logoColor=white'
+                alt='linkedin'
                 className='me-2'
               />
               <img
-                src='https://img.shields.io/badge/linkedin-%230077B5.svg?style=for-the-badge&logo=linkedin&logoColor=white'
-                alt='linkedin'
+                src='https://img.shields.io/badge/Facebook-%231877F2.svg?style=for-the-badge&logo=Facebook&logoColor=white'
+                alt='facebook'
                 className='me-2'
               />
             </div>
