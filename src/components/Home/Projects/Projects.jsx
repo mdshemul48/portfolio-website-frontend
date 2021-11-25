c3097b83e78184eb1cc8fed9a74d7b42baf76e36
MD. Shimul <mdshemul480@gmail.com>
2021-11-25 18:39:18 +0600

    adding project area

--- src/components/Home/Projects/Projects.jsx
+++ src/components/Home/Projects/Projects.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Button, Container } from 'react-bootstrap';
 
+import './Projects.css';
 const Projects = () => {
-  return <div>this is prject page</div>;
+  return (
+    <section className='mt-5'>
+      <Container>
+        <div className='d-flex justify-content-between'>
+          <h2 className=' project-title fw-bold'>Projects</h2>
+          <Button variant='outline-dark' size='sm'>
+            View More
+          </Button>
+        </div>
+        <hr />
+      </Container>
+    </section>
+  );
 };
 
 export default Projects;
