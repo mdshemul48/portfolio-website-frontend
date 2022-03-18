c2ac8b0ff459e2d3c2bb4bbc1579fc6721254547
MD. Shimul <mdshemul480@gmail.com>
2022-03-18 16:16:21 +0600

    adding projects page

--- src/components/Home/Projects/Projects.jsx
+++ src/components/Home/Projects/Projects.jsx
@@ -1,72 +1,10 @@
 import React from 'react';
 import { Container, Row } from 'react-bootstrap';
-
 import Project from './Project/Project';
 
+import projectDetails from './projectDetail';
+
 import './Projects.css';
-
-import ChakaPhoto from '../../../assets/projects/chaka.jpg';
-import HotelCaliforniaPhoto from '../../../assets/projects/hotel-california.jpg';
-import MallOnPhoto from '../../../assets/projects/MallOn.jpg';
-const projectDetails = [
-  {
-    title: 'Chaka - A Dream Car Project',
-    description:
-      'Users can create an order and give reviews from the User Dashboard. Admin control which gives access to all product managements and orders of all users. A dynamic dashboard for managing orders, adding products, and more.',
-    image: ChakaPhoto,
-    tags: [
-      'React',
-      'Node',
-      'Express',
-      'MongoDB',
-      'React-Router',
-      'Firebase-Auth',
-      'React-bootstrap',
-      'React-icons',
-      'heroku',
-    ],
-    previewLink: 'https://chaka101.web.app/',
-    githubClint: 'https://github.com/mdshemul48/Chaka-car-shop-frontend',
-    githubServer: 'https://github.com/mdshemul48/Chaka-car-shop-backend',
-  },
-  {
-    title: 'Hotel California - Hotel booking Site',
-    description:
-      'Customer reservations are added to the database and loaded separately for every user. Authorized clients can book and cancel reservations. Admin can add a new or delete an existing room.',
-    image: HotelCaliforniaPhoto,
-    tags: [
-      'Node',
-      'React',
-      'Express',
-      'MongoDB',
-      'React-Router',
-      'Firebase-Auth',
-      'React-bootstrap',
-      'heroku',
-    ],
-    previewLink: 'https://hotel-california101.web.app/',
-    githubClint: 'https://github.com/mdshemul48/Hotel-California-Frontend',
-    githubServer: 'https://github.com/mdshemul48/Hotel-California-backend',
-  },
-  {
-    title: 'MallOn - Medical & Healthcare',
-    description:
-      'MallOn is a professional dynamic single-page web application where users can view details of Healthcare products. Integrated with the private route where a user needs to login for view protected route. Authorized users can view product details information after login.',
-    image: MallOnPhoto,
-    tags: [
-      'React',
-      'React-Router',
-      'Firebase-Auth',
-      'React-bootstrap',
-      'react-icons',
-      'react-leaflet',
-      'axios',
-    ],
-    previewLink: 'https://mallmed-101.web.app/',
-    githubLink: 'https://github.com/mdshemul48/MallOn-Medical-Healthcare',
-  },
-];
-
 const Projects = () => {
   return (
     <section className='my-5'>
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Container, Row } from 'react-bootstrap';
+import Project from '../Home/Projects/Project/Project';
+
+import projectDetails from '../Home/Projects/projectDetail';
 
 const Projects = () => {
-  return <div>this is project page</div>;
+  return (
+    <section className='my-5'>
+      <Container>
+        <Row className='g-3'>
+          {projectDetails.map((project) => (
+            <Project project={project} key={project.title} />
+          ))}
+        </Row>
+      </Container>
+    </section>
+  );
 };
 
 export default Projects;
